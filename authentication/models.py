e5e6cbc7ae5716621052083d2146907e42b1bea3
darkodekan <darko.dekan@protonmail.com>
2021-01-30 17:15:08 +0100

    removed comment

--- authentication/models.py
+++ authentication/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from  django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password, first_name, last_name, is_admin=False):
         if not email:
